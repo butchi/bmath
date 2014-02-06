46834de4a9433d1e1b19407420af25bd1a41ccef
Yu(u)ki IWABUCHI <k38647+reg@gmail.com>
2014-02-06 18:39:32 +0900

    02

--- bmath.js
+++ bmath.js
@@ -1,4 +1,17 @@
 "use strict";
+/* 
+ * 現在2次元配列を想定
+ */
+function arrayQ(arr) {
+	var i;
+	var tmpLen=arr[0].length;
+	for(i=1; i<arr.length; i++) {
+		if(arr[i].length !==tmpLen) return false;
+		tmpLen = arr[i].length;
+	}
+	return true;
+}
+
 function first(arr) {
 	return arr[0];
 }
@@ -6,6 +19,19 @@ function first(arr) {
 function last(arr) {
 	return arr[arr.length-1];
 }
+
+function rest(arr) {
+}
+
+function most(arr) {
+}
+
+function take() {
+}
+
+function drop() {
+}
+
 
 function constantArray(c, len) {
 	var retArr = new Array(len);
@@ -16,6 +42,95 @@ function constantArray(c, len) {
 	return retArr;
 }
 
+function append() {
+}
+
+function prepend() {
+}
+
+function insert() {
+}
+
+function delete_() {
+}
+
+function appendTo() {
+}
+
+function prependTo() {
+}
+
+function position() {
+}
+
+function length(arr) {
+	return arr.length;
+}
+
+/*
+ * 現在1次元～2次元配列を想定
+ */
+function dimensions(arr) {
+	if(arrayQ(arr)) {
+		if(arr.length==0) {
+			return [0];
+		} else if(!(arr[0] instanceof Array)) {
+			return [arr.length, 1]
+		} else {
+			var x = arr[0].length;
+			var y = arr.length;
+			return [x, y];
+		}
+	} else {
+		return false;
+	}
+}
+
+function select_() {
+}
+
+function cases() {
+}
+
+function pick() {
+}
+
+function binLists() {
+}
+
+function flatten() {
+}
+
+function join() {
+}
+
+function union() {
+}
+
+function replacePart() {
+}
+
+function total() {
+}
+
+function accumulate() {
+}
+
+function differences() {
+}
+
+function max_() {
+}
+
+function min_() {
+}
+
+function sort_() {
+}
+
+function riffle() {
+}
+
 /*
 [
   [1,2,3],
@@ -24,6 +139,7 @@ function constantArray(c, len) {
 */
 
 function transpose(arr) {
+	/*
 	var i, j;
 	for(i=0; i<arr.length; i++) {
 		var tmp = arr[i];
@@ -31,4 +147,5 @@ function transpose(arr) {
 			if(j==0) var retArr = new Array();
 		}
 	}
+	*/
 }
