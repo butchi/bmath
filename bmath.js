7a78435ba0f2aee18991f04c01df3ede27f5701b
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2014-02-06 19:30:37 +0900

    記法を変更

--- bmath.js
+++ bmath.js
@@ -1,367 +1,388 @@
-"use strict";
-/* 
- * 現在2次元配列を想定
- */
-function arrayQ(arr) {
-	var i;
-	var tmpLen=arr[0].length;
-	for(i=1; i<arr.length; i++) {
-		if(arr[i].length !==tmpLen) return false;
-		tmpLen = arr[i].length;
-	}
-	return true;
-}
+(function() {
+  "use strict";
+  window.bMath = window.bMath || {};
+  /* 
+   * 現在2次元配列を想定
+   */
+  bMath.arrayQ = function(arr) {
+    var i;
+    var tmpLen=arr[0].length;
+    for(i=1; i<arr.length; i++) {
+      if(arr[i].length !==tmpLen) return false;
+      tmpLen = arr[i].length;
+    }
+    return true;
+  }
 
-/*
- * プリミティブ型および配列が同じかどうか比較
- */
-function sameQ(arg1, arg2) {
-	if(arg1 === arg2) {
-		return true;
-	}
-	if(arg1 instanceof Array && arg2 instanceof Array) {
-		var len = arg1.length;
-		if(len !== arg2.length) {
-			return false;
-		}
-		var i;
-		for(i=0; i<len; i++) {
-			if(!sameQ(arg1[i], arg2[i])) {
-				return false;
-			}
-		}
-		return true;
-	} else {
-		return false;
-	}
-}
+  /*
+   * プリミティブ型および配列が同じかどうか比較
+   */
+  bMath.sameQ = function(arg1, arg2) {
+    if(arg1 === arg2) {
+      return true;
+    }
+    if(arg1 instanceof Array && arg2 instanceof Array) {
+      var len = arg1.length;
+      if(len !== arg2.length) {
+        return false;
+      }
+      var i;
+      for(i=0; i<len; i++) {
+        if(!bMath.sameQ(arg1[i], arg2[i])) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-function first(arr) {
-	return arr[0];
-}
+  bMath.first = function(arr) {
+    return arr[0];
+  }
 
-function last(arr) {
-	return arr[arr.length-1];
-}
+  bMath.last = function(arr) {
+    return arr[arr.length-1];
+  }
 
-function rest(arr) {
-	return arr.slice(1);
-}
+  bMath.rest = function(arr) {
+    return arr.slice(1);
+  }
 
-function most(arr) {
-	return 	arr.slice(0, arr.length-1);
-}
+  bMath.most = function(arr) {
+    return arr.slice(0, arr.length-1);
+  }
 
-function take(arr, n) {
-	if(n>=0) {
-		return arr.slice(0, n);
-	} else {
-		return arr.slice(n);
-	}
-}
+  bMath.take = function(arr, n) {
+    if(n>=0) {
+      return arr.slice(0, n);
+    } else {
+      return arr.slice(n);
+    }
+  }
 
-function drop(arr, n) {
-	if(n>=0) {
-		return arr.slice(n);
-	} else {
-		if(-n>arr.length) {
-			return [];
-		} else {
-			return arr.slice(0,arr.length+n);
-		}
-	}
-}
+  bMath.drop = function(arr, n) {
+    if(n>=0) {
+      return arr.slice(n);
+    } else {
+      if(-n>arr.length) {
+        return [];
+      } else {
+        return arr.slice(0,arr.length+n);
+      }
+    }
+  }
 
 
-function constantArray(c, len) {
-	var retArr = new Array(len);
-	var i;
-	for(i=0; i<len; i++) {
-		retArr[i] = c;
-	}
-	return retArr;
-}
+  bMath.constantArray = function(c, len) {
+    var retArr = new Array(len);
+    var i;
+    for(i=0; i<len; i++) {
+      retArr[i] = c;
+    }
+    return retArr;
+  }
 
-function range(n) {
-	var ret = [];
-	var i;
-	for(i=0; i<n; i++) {
-		ret.push(i);
-	}
-	return ret;
-}
+  bMath.range = function(n) {
+    var ret = [];
+    var i;
+    for(i=0; i<n; i++) {
+      ret.push(i);
+    }
+    return ret;
+  }
 
-function table() {
-}
+  /*
+  bMath.table = function() {
+  }
 
-function append() {
-}
+  bMath.append = function() {
+  }
 
-function prepend() {
-}
+  bMath.prepend = function() {
+  }
 
-function insert() {
-}
+  bMath.insert = function() {
+  }
 
-function delete_() {
-}
+  bMath.delete = function() {
+  }
 
-function appendTo() {
-}
+  bMath.appendTo = function() {
+  }
 
-function prependTo() {
-}
+  bMath.prependTo = function() {
+  }
+  */
 
-function position(arr, x) {
-	var ret = [];
-	var len = arr.length;
-	var i;
-	for(i=0; i<len; i++) {
-		if(sameQ(arr[i], x)) {
-			ret.push(i);
-		}
-	}
-	return ret;
-}
+  bMath.position = function(arr, x) {
+    var ret = [];
+    var len = arr.length;
+    var i;
+    for(i=0; i<len; i++) {
+      if(bMath.sameQ(arr[i], x)) {
+        ret.push(i);
+      }
+    }
+    return ret;
+  }
 
-function length(arr) {
-	return arr.length;
-}
+  bMath.length = function(arr) {
+    return arr.length;
+  }
 
-/*
- * 現在1次元～2次元配列を想定
- */
-function dimensions(arr) {
-	if(arrayQ(arr)) {
-		if(arr.length==0) {
-			return [0];
-		} else if(!(arr[0] instanceof Array)) {
-			return [arr.length, 1]
-		} else {
-			var x = arr[0].length;
-			var y = arr.length;
-			return [x, y];
-		}
-	} else {
-		return false;
-	}
-}
+  /*
+   * 現在1次元～2次元配列を想定
+   */
+  bMath.dimensions = function(arr) {
+    if(bMath.arrayQ(arr)) {
+      if(arr.length==0) {
+        return [0];
+      } else if(!(arr[0] instanceof Array)) {
+        return [arr.length, 1]
+      } else {
+        var x = arr[0].length;
+        var y = arr.length;
+        return [x, y];
+      }
+    } else {
+      return false;
+    }
+  }
 
-function select() {
-}
+  /*
+  bMath.select = function() {
+  }
 
-function cases() {
-}
+  bMath.cases = function() {
+  }
 
-function pick() {
-}
+  bMath.pick = function() {
+  }
 
-function binLists() {
-}
+  bMath.binLists = function() {
+  }
+  */
 
-function flatten(arr) {
-	if(arr instanceof Array) {
-		var ret = [];
-		var len = arr.length;
-		var i;
-		for(i=0; i<len; i++) {
-			ret = ret.concat(flatten(arr[i]));
-		}
-		return ret;
-	} else {
-		return arr;
-	}
-}
+  bMath.flatten = function(arr) {
+    if(arr instanceof Array) {
+      var ret = [];
+      var len = arr.length;
+      var i;
+      for(i=0; i<len; i++) {
+        ret = ret.concat(flatten(arr[i]));
+      }
+      return ret;
+    } else {
+      return arr;
+    }
+  }
 
-function arrayFlatten() {
-}
+  /*
+  bMath.arrayFlatten = function() {
+  }
+  */
 
-function join(arr1, arr2) {
-	return [].concat.apply([], arguments);
-}
+  bMath.join = function(arr1, arr2) {
+    return [].concat.apply([], arguments);
+  }
 
-/*
- * リスト list を一連の同一要素からなるサブリストに分割する．
- */
-function split() {
-}
+  /*
+   * リスト list を一連の同一要素からなるサブリストに分割する．
+   */
+   /*
+  bMath.split = function() {
+  }
+  */
 
-/**
- * from http://code.jquery.com/jquery-1.8.2.js Sizzle.uniqueSort
- */
-function union(results) {
-	var elem,
-		i = 1;
-	
-	//hasDuplicate = baseHasDuplicate;
-	//results.sort( sortOrder );
-	results.sort( function(a,b) {return a-b} );
-	
-	//if ( hasDuplicate ) {
-		for ( ; (elem = results[i]); i++ ) {
-			if ( elem === results[ i - 1 ] ) {
-				results.splice( i--, 1 );
-			}
-		}
-	//}
-	
-	return results;
-}
+  /**
+   * from http://code.jquery.com/jquery-1.8.2.js Sizzle.uniqueSort
+   */
+  bMath.union = function(results) {
+    var elem,
+      i = 1;
 
-function deleteDuplicates() {
-}
+    //hasDuplicate = baseHasDuplicate;
+    //results.sort( sortOrder );
+    results.sort( function(a,b) {return a-b} );
 
-/*
- * Mathematica: list を重複しない長さ n のサブリストに分割する．
- */
-function partition(arr, n) {
-}
+    //if ( hasDuplicate ) {
+      for ( ; (elem = results[i]); i++ ) {
+        if ( elem === results[ i - 1 ] ) {
+          results.splice( i--, 1 );
+        }
+      }
+    //}
 
-function replacePart() {
-}
+    return results;
+  }
 
-function total(arr) {
-	// 数値の配列という前提
-	var i;
-	var len = arr.length;
-	var ret = 0;
-	for(i=0; i<len; i++) {
-		ret += arr[i]
-	}
-	return ret;
-}
+  bMath.deleteDuplicates = function() {
+  }
 
-function accumulate(arr) {
-	// 数値の配列という前提
-	var i;
-	var len = arr.length;
-	var ret = new Array(len);
-	ret[0] = arr[0];
-	for(i=1; i<len; i++) {
-		ret[i] = ret[i-1] + arr[i];
-	}
-	return ret;
-}
+  /*
+   * Mathematica: list を重複しない長さ n のサブリストに分割する．
+   */
+   /*
+  bMath.partition = function(arr, n) {
+  }
+  */
 
-function differences(arr) {
-	// 数値の配列という前提
-	var i;
-	var len = arr.length;
-	var ret = new Array(len-1);
-	for(i=0; i<len-1; i++) {
-		ret[i] = arr[i+1] - arr[i];
-	}
-	return ret;
-}
+  /*
+  bMath.replacePart = function() {
+  }
+  */
 
-function max(arr) {
-	// 数値の配列という前提
-	return Math.max.apply(null, arr);
-}
+  bMath.total = function(arr) {
+    // 数値の配列という前提
+    var i;
+    var len = arr.length;
+    var ret = 0;
+    for(i=0; i<len; i++) {
+      ret += arr[i]
+    }
+    return ret;
+  }
 
-function min(arr) {
-	// 数値の配列という前提
-	return Math.min.apply(null, arr);
-}
+  bMath.accumulate = function(arr) {
+    // 数値の配列という前提
+    var i;
+    var len = arr.length;
+    var ret = new Array(len);
+    ret[0] = arr[0];
+    for(i=1; i<len; i++) {
+      ret[i] = ret[i-1] + arr[i];
+    }
+    return ret;
+  }
 
-function mean(arr) {
-	// 数値の配列という前提
-	return total(arr)/arr.length;
-}
+  bMath.differences = function(arr) {
+    // 数値の配列という前提
+    var i;
+    var len = arr.length;
+    var ret = new Array(len-1);
+    for(i=0; i<len-1; i++) {
+      ret[i] = arr[i+1] - arr[i];
+    }
+    return ret;
+  }
 
-function sort() {
-}
+  bMath.max = function(arr) {
+    // 数値の配列という前提
+    return Math.max.apply(null, arr);
+  }
 
-function riffle() {
-}
+  bMath.min = function(arr) {
+    // 数値の配列という前提
+    return Math.min.apply(null, arr);
+  }
 
-function padRight(arr, n, c) {
-	if(typeof c == "undefined") {
-		c = 0;
-	}
-	if(n>=arr.length) {
-		return arr.concat(constantArray(c, n-arr.length));
-	} else {
-		return arr.slice(0, n);
-	}
-}
+  bMath.mean = function(arr) {
+    // 数値の配列という前提
+    return bMath.total(arr)/arr.length;
+  }
 
-function padLeft(arr, n, c) {
-	if(typeof c == "undefined") {
-		c = 0;
-	}
-	if(n>=arr.length) {
-		return constantArray(c, n-arr.length).concat(arr);
-	} else {
-		return arr.slice(-n);
-	}
-}
+  /*
+  bMath.sort = function() {
+  }
 
-function rotateRight(arr, n) {
-	if(typeof n == "undefined") {
-		n = 1;
-	}
-	if(n<0) {
-		return rotateLeft(arr, -n);
-	}
-	n = n % arr.length
-	return arr.slice(-n).concat(arr.slice(0,arr.length-n));
-}
+  bMath.riffle = function() {
+  }
+  */
 
-function rotateLeft(arr, n) {
-	if(typeof n == "undefined") {
-		n = 1;
-	}
-	if(n<0) {
-		return rotateRight(arr, -n);
-	}
-	n = n % arr.length
-	return arr.slice(n).concat(arr.slice(0,n));
-}
+  bMath.padRight = function(arr, n, c) {
+    if(typeof c == "undefined") {
+      c = 0;
+    }
+    if(n>=arr.length) {
+      return arr.concat(constantArray(c, n-arr.length));
+    } else {
+      return arr.slice(0, n);
+    }
+  }
 
-/*
-[
-  [1,2,3],
-  [4,5,6]
-]
-*/
+  bMath.padLeft = function(arr, n, c) {
+    if(typeof c == "undefined") {
+      c = 0;
+    }
+    if(n>=arr.length) {
+      return bMath.constantArray(c, n-arr.length).concat(arr);
+    } else {
+      return arr.slice(-n);
+    }
+  }
 
-function transpose(arr) {
-	/*
-	var i, j;
-	for(i=0; i<arr.length; i++) {
-		var tmp = arr[i];
-		for(j=0; j<arr[i].length; j++) {
-			if(j==0) var retArr = new Array();
-		}
-	}
-	*/
-}
+  bMath.rotateRight = function(arr, n) {
+    if(typeof n == "undefined") {
+      n = 1;
+    }
+    if(n<0) {
+      return bMath.rotateLeft(arr, -n);
+    }
+    n = n % arr.length;
+    return arr.slice(-n).concat(arr.slice(0,arr.length-n));
+  }
 
-function map(f, arr) {
-	var ret = [];
-	var len = arr.length;
-	var i;
-	if(arr instanceof Array) {
-		for(i=0; i<len; i++) {
-			ret.push(f(arr[i]));
-		}
-	}
-	return ret;
-}
+  bMath.rotateLeft = function(arr, n) {
+    if(typeof n == "undefined") {
+      n = 1;
+    }
+    if(n<0) {
+      return bMath.rotateRight(arr, -n);
+    }
+    n = n % arr.length
+    return arr.slice(n).concat(arr.slice(0,n));
+  }
 
-function mapAll(f, arr) {
-	var ret = [];
-	var len = arr.length;
-	var i;
-	if(arr instanceof Array) {
-		for(i=0; i<len; i++) {
-			if(arr[i] instanceof Array) {
-				ret.push(mapAll(f, arr[i]));
-			} else {
-				ret.push(f(arr[i]));
-			}
-		}
-	}
-	return ret;
-}
+  /*
+  [
+    [1,2,3],
+    [4,5,6]
+  ]
+  */
+  /*
+  bMath.transpose = function(arr) {
+    var i, j;
+    for(i=0; i<arr.length; i++) {
+      var tmp = arr[i];
+      for(j=0; j<arr[i].length; j++) {
+        if(j==0) var retArr = new Array();
+      }
+    }
+  }
+  */
+
+  bMath.map = function(f, arr) {
+    var ret = [];
+    var len = arr.length;
+    var i;
+    if(arr instanceof Array) {
+      for(i=0; i<len; i++) {
+        ret.push(f(arr[i]));
+      }
+    }
+    return ret;
+  }
+
+  bMath.mapAll = function(f, arr) {
+    var ret = [];
+    var len = arr.length;
+    var i;
+    if(arr instanceof Array) {
+      for(i=0; i<len; i++) {
+        if(arr[i] instanceof Array) {
+          ret.push(bMath.mapAll(f, arr[i]));
+        } else {
+          ret.push(f(arr[i]));
+        }
+      }
+    }
+    return ret;
+  }
+
+  /*
+  bMath.fourier = function(arr) {
+  }
+  */
+}());
