08bc2cde760a5fbef93287a514d6ed899b38e1fc
Yu(u)ki IWABUCHI <k38647+reg@gmail.com>
2014-02-06 18:41:03 +0900

    05

--- bmath.js
+++ bmath.js
@@ -12,7 +12,28 @@ function arrayQ(arr) {
 	return true;
 }
 
-function sameQ() {
+/*
+ * プリミティブ型および配列が同じかどうか比較
+ */
+function sameQ(arg1, arg2) {
+	if(arg1 === arg2) {
+		return true;
+	}
+	if(arg1 instanceof Array && arg2 instanceof Array) {
+		var len = arg1.length;
+		if(len !== arg2.length) {
+			return false;
+		}
+		var i;
+		for(i=0; i<len; i++) {
+			if(!sameQ(arg1[i], arg2[i])) {
+				return false;
+			}
+		}
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function first(arr) {
@@ -28,13 +49,27 @@ function rest(arr) {
 }
 
 function most(arr) {
-	return 	arr.slice(0,arr.length-1);
+	return 	arr.slice(0, arr.length-1);
 }
 
-function take() {
+function take(arr, n) {
+	if(n>=0) {
+		return arr.slice(0, n);
+	} else {
+		return arr.slice(n);
+	}
 }
 
-function drop() {
+function drop(arr, n) {
+	if(n>=0) {
+		return arr.slice(n);
+	} else {
+		if(-n>arr.length) {
+			return [];
+		} else {
+			return arr.slice(0,arr.length+n);
+		}
+	}
 }
 
 
@@ -45,6 +80,18 @@ function constantArray(c, len) {
 		retArr[i] = c;
 	}
 	return retArr;
+}
+
+function range(n) {
+	var ret = [];
+	var i;
+	for(i=0; i<n; i++) {
+		ret.push(i);
+	}
+	return ret;
+}
+
+function table() {
 }
 
 function append() {
@@ -65,7 +112,16 @@ function appendTo() {
 function prependTo() {
 }
 
-function position() {
+function position(arr, x) {
+	var ret = [];
+	var len = arr.length;
+	var i;
+	for(i=0; i<len; i++) {
+		if(sameQ(arr[i], x)) {
+			ret.push(i);
+		}
+	}
+	return ret;
 }
 
 function length(arr) {
@@ -91,7 +147,7 @@ function dimensions(arr) {
 	}
 }
 
-function select_() {
+function select() {
 }
 
 function cases() {
@@ -103,10 +159,25 @@ function pick() {
 function binLists() {
 }
 
-function flatten() {
+function flatten(arr) {
+	if(arr instanceof Array) {
+		var ret = [];
+		var len = arr.length;
+		var i;
+		for(i=0; i<len; i++) {
+			ret = ret.concat(flatten(arr[i]));
+		}
+		return ret;
+	} else {
+		return arr;
+	}
 }
 
-function join() {
+function arrayFlatten() {
+}
+
+function join(arr1, arr2) {
+	return [].concat.apply([], arguments);
 }
 
 /*
@@ -135,6 +206,9 @@ function union(results) {
 	//}
 	
 	return results;
+}
+
+function deleteDuplicates() {
 }
 
 /*
@@ -180,18 +254,69 @@ function differences(arr) {
 	return ret;
 }
 
-function max_(ret) {
+function max(arr) {
 	// 数値の配列という前提
+	return Math.max.apply(null, arr);
 }
 
-function min_() {
+function min(arr) {
 	// 数値の配列という前提
+	return Math.min.apply(null, arr);
 }
 
-function sort_() {
+function mean(arr) {
+	// 数値の配列という前提
+	return total(arr)/arr.length;
+}
+
+function sort() {
 }
 
 function riffle() {
+}
+
+function padRight(arr, n, c) {
+	if(typeof c == "undefined") {
+		c = 0;
+	}
+	if(n>=arr.length) {
+		return arr.concat(constantArray(c, n-arr.length));
+	} else {
+		return arr.slice(0, n);
+	}
+}
+
+function padLeft(arr, n, c) {
+	if(typeof c == "undefined") {
+		c = 0;
+	}
+	if(n>=arr.length) {
+		return constantArray(c, n-arr.length).concat(arr);
+	} else {
+		return arr.slice(-n);
+	}
+}
+
+function rotateRight(arr, n) {
+	if(typeof n == "undefined") {
+		n = 1;
+	}
+	if(n<0) {
+		return rotateLeft(arr, -n);
+	}
+	n = n % arr.length
+	return arr.slice(-n).concat(arr.slice(0,arr.length-n));
+}
+
+function rotateLeft(arr, n) {
+	if(typeof n == "undefined") {
+		n = 1;
+	}
+	if(n<0) {
+		return rotateRight(arr, -n);
+	}
+	n = n % arr.length
+	return arr.slice(n).concat(arr.slice(0,n));
 }
 
 /*
@@ -211,4 +336,32 @@ function transpose(arr) {
 		}
 	}
 	*/
+}
+
+function map(f, arr) {
+	var ret = [];
+	var len = arr.length;
+	var i;
+	if(arr instanceof Array) {
+		for(i=0; i<len; i++) {
+			ret.push(f(arr[i]));
+		}
+	}
+	return ret;
+}
+
+function mapAll(f, arr) {
+	var ret = [];
+	var len = arr.length;
+	var i;
+	if(arr instanceof Array) {
+		for(i=0; i<len; i++) {
+			if(arr[i] instanceof Array) {
+				ret.push(mapAll(f, arr[i]));
+			} else {
+				ret.push(f(arr[i]));
+			}
+		}
+	}
+	return ret;
 }
